8df231ab88aa27b9c1252b8bbf4cfa447b491158
Christophe Malaboeuf <christophe.malaboeuf@gmail.com>
2017-10-02 20:18:08 +0200

    feat: Adapt getMessages with observable

--- src/app/input-message/input-message.component.spec.ts
+++ src/app/input-message/input-message.component.spec.ts
@@ -1,16 +1,26 @@
+import { Observable } from 'rxjs/Observable';
 import { MessageService } from './../services/message.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputMessageComponent } from './input-message.component';
 
-describe('InputMessageComponent', () => {
+describe('MessageInputComponent', () => {
   let component: InputMessageComponent;
   let fixture: ComponentFixture<InputMessageComponent>;
 
+
+  const messageServiceStub = {
+    getMessages()  {
+      return Observable.create(observer => {
+        observer.next([{ id: 1, content: 'fake message', author: 'cri' }]);
+        observer.complete();
+      });
+    }
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [InputMessageComponent],
-      providers : [MessageService]
+      providers: [{provide: MessageService, useValue: messageServiceStub}]
     })
     .compileComponents();
   }));
--- src/app/services/message.service.spec.ts
+++ src/app/services/message.service.spec.ts
@@ -1,25 +1,63 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { TestBed, inject, async, fakeAsync, tick } from '@angular/core/testing';
+import { HttpModule, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { MessageService } from './message.service';
-
 describe('MessageService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MessageService]
-    });
-  });
-
   it('should be created', inject([MessageService], (service: MessageService) => {
     expect(service).toBeTruthy();
   }));
+  const API_URL: String = 'http://example.com';
+  let httpStub;
+  let service: MessageService;
+  beforeEach(() => {
+    httpStub = {
+      get: () => {
+        return Observable.create(observer => {
+          observer.next({ json: () => ([{ id: 1, content: 'fake content', author: 'fhi' }]) });
+          observer.complete();
+        });
+      },
+      post: () => {
+        return Observable.create(observer => {
+          observer.next({ json: () => ({ id: 1, content: 'hello world', author: 'abc' })});
+          observer.complete();
+        });
+      }
+    };
 
-  it('#getMessages should return array of messages', inject([MessageService], (service: MessageService) => {
-    service.getMessages();
-    expect(service.messages.length).toBe(2);
-  }));
+    TestBed.configureTestingModule({
+      providers: [
+        MessageService,
+        { provide: Http, useValue: httpStub }
+      ]
+    });
+    service = TestBed.get(MessageService);
+  });
 
-  it('#getMessages should return array of messages', inject([MessageService], (service: MessageService) => {
-    service.createMessage({id: 3, author: 'Test', content: 'test'});
-    expect(service.messages.length).toBe(3);
-  }));
+  it('should inject message service', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should have messageUrl property set', () => {
+    expect(service.messagesUrl).toEqual('http://microblog-api.herokuapp.com/api/messages');
+  });
+
+  it('should set messages array to empty by default', () => {
+    expect(service.messages).toEqual([]);
+  });
+
+  it('#getMessages should fetch messages and set `messages` property', fakeAsync(inject([Http],
+    (http: Http) => {
+
+      service.messagesUrl = 'http://fake.base.url';
+
+      const spy = spyOn(http, 'get').and.callThrough();
+      tick();
+      service.getMessages().subscribe(response => {
+        expect(spy).toHaveBeenCalledWith('http://fake.base.url');
+        expect(response).toEqual([{ id: 1, content: 'fake content', author: 'fhi' }]);
+      });
+    })));
 });
